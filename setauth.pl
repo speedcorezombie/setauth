317dfbf423a8f1d195e12706522d86843a65c0a3
speedcorezombie <speedcorezombie@mail.com>
2013-08-12 18:30:30 +0400

    add setauth.pl repeat

--- setauth.pl
+++ setauth.pl
@@ -32,22 +32,21 @@ my $document_root;
 $httpd_conf = "/usr/local/apache/conf/httpd.conf";                                                                                                          
                                                                                                                                                             
 # Parse configuration                                                                                                                                       
-$apache_conf = Apache::ConfigFile->read(file => $httpd_conf,                                                                                                 
-                                                                                ignore_case  => 1,
-                                                                                expand_vars  => 1,
-                                                                                fix_booleans => 1
-                                );
+$apache_conf = Apache::ConfigFile->read(file => $httpd_conf,
+										ignore_case  => 1,
+										expand_vars  => 1,
+										fix_booleans => 1
+				);
 
 # Get array of VirtualHost ips and process each
 @vhosts_ip = $apache_conf->cmd_context('VirtualHost');
 foreach my $vhip (@vhosts_ip) {
-                # Get array of VirtualHost on this ip
-                @vhosts = $apache_conf->cmd_context(VirtualHost => $vhip);
-                foreach $vhost_context (@vhosts) {
-                                $server_name   = $vhost_context->cmd_config('ServerName');
-                                @server_alias  = $vhost_context->cmd_config('ServerAlias');
-                                $document_root = $vhost_context->cmd_config('DocumentRoot');
-                }
+	# Get array of VirtualHost on this ip
+	@vhosts = $apache_conf->cmd_context(VirtualHost => $vhip);
+	foreach $vhost_context (@vhosts) {
+		$server_name   = $vhost_context->cmd_config('ServerName');
+		@server_alias  = $vhost_context->cmd_config('ServerAlias');
+		$document_root = $vhost_context->cmd_config('DocumentRoot');
+	}
 }
-}
-}
+
