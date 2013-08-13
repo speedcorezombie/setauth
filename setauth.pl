31ab8f900996ff83f21ea67ead4c8d725bfc7da2
speedcorezombie <speedcorezombie@mail.com>
2013-08-13 14:46:35 +0400

    add auth section

--- setauth.pl
+++ setauth.pl
@@ -35,6 +35,12 @@ my $htaccess;
 # Auth present flag
 my $auth_present;
 
+# Path to .htpasswd
+my $htpasswd_path;
+
+# Authentiation directives
+my $auth = "AuthType Basic\nAuthName \"Administration zone\"\nAuthUserFile \"$htpasswd_path/.htpasswd\"\nRequire valid-user\n"
+
 # Set admin dir path (relative)
 $admin_path = "administrator";
 
